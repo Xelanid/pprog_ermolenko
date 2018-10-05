5aa2c07066460a1b185ca6d51411265895d15824
Xelanid <ermolenko.alexander@gmail.com>
2018-10-05 16:50:17 +0500

    Теперь я хочу сделать фонарь живым, поэтому ввожу функцию глаз

--- structuraris.py
+++ structuraris.py
@@ -19,3 +19,17 @@ def palka(x, y):
     stick = gr.Polygon(gr.Point(x - 3, y / 2), gr.Point(x - 3, y), gr.Point(x + 3, y), gr.Point(x + 3, y / 2))
     stick.setFill('black')
     stick.draw(window)
+
+def glaza(x, y):
+    eye1 = gr.Circle(gr.Point(x - 25, y - 15), 10)
+    eye2 = gr.Circle(gr.Point(x + 22, y - 19), 15)
+    eye1_center = gr.Circle(gr.Point(x - 25, y - 15), 5)
+    eye2_center = gr.Circle(gr.Point(x + 22, y - 19), 7)
+    eye1.setFill('red')
+    eye2.setFill('red')
+    eye1_center.setFill('black')
+    eye2_center.setFill('black')
+    eye1.draw(window)
+    eye2.draw(window)
+    eye1_center.draw(window)
+    eye2_center.draw(window)
