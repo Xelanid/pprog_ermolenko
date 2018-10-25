3c988d01c6a5473e51f7cb4b37080d069a5e634a
Xelanid <ermolenko.alexander@gmail.com>
2018-10-26 00:24:55 +0300

    Появление шариков в месте нажатия

--- catch_balls.py
+++ catch_balls.py
@@ -9,24 +9,19 @@ canv.pack(fill = BOTH, expand = 1)
  
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'gray', 'black']
 
-def new_ball():
-    canv.delete(ALL)
-    x = rnd(100,700)
-    y = rnd(100,500)
-    r = rnd(30,50)
-    canv.create_oval(x - r, y - r, x + r, y + r, fill = choice(colors), width = 0)
-    root.after(1000, new_ball)
-
 def left_click(event):
     print('left click')
+    x = event.x
+    y = event.y
+    r = rnd(30,50)
+    canv.create_oval(x - r, y - r, x + r, y + r, fill = choice(colors), width = 0)
     
 
 def right_click(event):
     print('right click')
+    canv.delete(ALL)
 
 canv.bind('<Button-1>', left_click)
 canv.bind('<Button-3>', right_click)
-    
-     
-new_ball()
+
 mainloop()
