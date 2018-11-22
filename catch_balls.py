59e4df660eedd51bec23c4d2545ebe31373b3c16
Xelanid <ermolenko.alexander@gmail.com>
2018-11-23 02:20:29 +0300

    Пофиксил

--- catch_balls.py
+++ catch_balls.py
@@ -1,16 +1,20 @@
 from tkinter import *
 from random import randrange as rnd, choice
-import time
+import csv
+ 
+FILENAME = "Top players.csv"
 root = Tk()
 root.geometry('800x600')
 
 points = 0
 points1 = 0
  
-canv = Canvas(root, bg = 'white')
-canv.pack(fill = BOTH, expand = 1) 
+canv = Canvas(root, bg='white')
+canv.pack(fill=BOTH, expand=1)
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'gray', 'black']
 
+print('Please enter your name')
+name = input()
 
 
 def new_ball():
@@ -20,8 +24,9 @@ def new_ball():
     y = rnd(100,500)
     r = rnd(30,50)
     
-    circle = canv.create_oval(x - r, y - r, x + r, y + r, fill = choice(colors), width = 0)
-    root.after(10000, new_ball)
+    circle = canv.create_oval(x - r, y - r, x + r, y + r, fill=choice(colors), width = 0)
+    root.after(1000, new_ball)
+
 
 def hit(event):
     global points, result, y, result1, points1
@@ -33,14 +38,20 @@ def hit(event):
     canv.delete(result)
     canv.delete(result1)
     canv.delete(circle)
-    result = canv.create_text(180, 20, text = 'Сколько забьет Заболотный: ' + str(points), font = 'Calibri 20')
-    result1 = canv.create_text(200, 40, text = 'Сколько пропустит Максименко: ' + str(points1), font = 'Calibri 20')
+    result = canv.create_text(180, 20, text='Сколько забьет Заболотный: ' + str(points), font='Calibri 20')
+    result1 = canv.create_text(200, 40, text='Сколько пропустит Максименко: ' + str(points1), font='Calibri 20')
 
-circle = canv.create_oval(-100,0,0,0)
-result = canv.create_text(180, 20, text = 'Сколько забьет Заболотный: ' + str(points), font = 'Calibri 20')
-result1 = canv.create_text(200, 40, text = 'Сколько пропустит Максименко: ' + str(points1), font = 'Calibri 20')
+
+circle = canv.create_oval(-100, 0, 0, 0)
+result = canv.create_text(180, 20, text='Сколько забьет Заболотный: ' + str(points), font='Calibri 20')
+result1 = canv.create_text(200, 40, text='Сколько пропустит Максименко: ' + str(points1), font='Calibri 20')
 points = 0
 points1 = 0
 canv.bind('<Button-1>', hit)    
 new_ball()
 mainloop()
+
+with open(FILENAME, "a", newline="") as file:
+    player = [name, points]
+    writer = csv.writer(file)
+    writer.writerow(player)
