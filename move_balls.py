7d5d9224d209cb84fdd4a077fdf3d4a67a570694
Xelanid <ermolenko.alexander@gmail.com>
2018-11-23 12:31:34 +0300

    Сделяль

--- move_balls.py
+++ move_balls.py
@@ -4,45 +4,73 @@ from random import randrange as rnd, choice
 root = Tk()
 root.geometry('800x600')
 width = 600
-height = 800
-
+height = 600
 canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'gray', 'black']
-qty = rnd(3, 10)
+qty = rnd(7, 15)
 balls = []
 r = rnd(10, 30)
 
 
-for i in range(qty):
+for j in range(qty):
     x = rnd(r, width - r)
     y = rnd(r, height - r)
     dx = rnd(1, 10)
     dy = rnd(1, 10)
     circle = canv.create_oval(x - r, y - r, x + r, y + r, fill=choice(colors))
-    ball = [x, y, dx, dy, r, circle]
+    ball = [x, y, dx, dy, circle]
     balls.append(ball)
 
 
-x0 = rnd(100, 700)
-y0 = rnd(100, 500)
+def tick_handler():
+    global balls
+    for i in range(len(balls)):
+        x_i, y_i, dx_i, dy_i, circle_i = balls[i]
+        x_i += dx_i
+        y_i += dy_i
+        if x_i < 0:
+            dx_i = -dx_i
+            x_i = 0
+        elif x_i > width - r:
+            dx_i = -dx_i
+            x_i = width - r
+        if y_i < 0:
+            dy_i = -dy_i
+            y_i = 0
+        elif y_i > height - r:
+            dy_i = -dy_i
+            y_i = height - r
+        balls[i] = [x_i, y_i, dx_i, dy_i, circle_i]
+        canv.move(circle_i, dx_i, dy_i)
 
 
-def balls():
-    global x0, y0, circle, x, y
-    x0 += x
-    y0 += y
-    if x0 > 750 or x0 < 50:
-        x *= -1
-    if y0 > 550 or y0 < 50:
-        y *= -1
+def time_handler():
+    global freeze
+    speed = speed_scale.get()
+    if speed == 0:
+        freeze = True
+        return
+    tick_handler()
+    sleep = 1100 - 100 * speed
+    root.after(sleep, time_handler)
 
 
-canv.delete(circle)
-circle = canv.create_oval(x0 - r, y0 - r, x0 + r, y0 + r, fill='yellow', width=0)
-root.after(15, balls)
-circle = canv.create_oval(-100, 0, 0, 0)
-balls()
-x = 2
-y = 1
-mainloop()
+def unfreezer(event):
+    global freeze
+    if freeze:
+        speed = speed_scale.get()
+        if speed != 0:
+            freeze = False
+            root.after(0, time_handler)
+
+
+speed_scale = Scale(root, orient=HORIZONTAL, length=300, from_=0, to=10, tickinterval=1, resolution=1)
+
+speed_scale.pack()
+speed_scale.set(1)
+freeze = False
+root.after(10, time_handler)
+speed_scale.bind("<Motion>", unfreezer)
+
+root.mainloop()
